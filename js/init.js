308246e4a16136628f500785b57476e287495271
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-04-12 11:51:47 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyDTORhF11V8eCGH07xcgfStMM3VzLa-xtk",
+ apiKey: "AIzaSyDTORhF11V8eCGH07xcgfStMM3VzLa-xtk",
     authDomain: "evaluacion3erparcial.firebaseapp.com",
+    databaseURL: "https://evaluacion3erparcial-default-rtdb.firebaseio.com",
     projectId: "evaluacion3erparcial",
     storageBucket: "evaluacion3erparcial.appspot.com",
     messagingSenderId: "811857773266",
