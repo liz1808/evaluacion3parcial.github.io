704ae043e61de7cf892b95e878e8fed0850d7363
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-04-12 09:07:55 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDrciNVISy1Q98NoTa5e4H_Uk_uoDxmDg4",
+    authDomain: "bdalum41.firebaseapp.com",
+    projectId: "bdalum41",
+    storageBucket: "bdalum41.appspot.com",
+    messagingSenderId: "225742393346",
+    appId: "1:225742393346:web:9550ccfabeaf990244d71b",
+    measurementId: "G-YKVKZMJ5X2"
+});
